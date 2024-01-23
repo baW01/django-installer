5be2ff04322570cc2377f11edfafacda9cfc9b66
baWsiedobrze <82673494+baW01@users.noreply.github.com>
2024-01-23 18:56:42 +0100

    Update django-installer.py

--- django-installer.py
+++ django-installer.py
@@ -3,16 +3,13 @@ import sys
 import os
 
 def install_django():
-    # Instalacja Django
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'django'])
 
-    # Dodanie Django do zmiennej środowiskowej PATH
     python_path = os.path.dirname(sys.executable)
     scripts_path = os.path.join(python_path, 'Scripts')
     if scripts_path not in os.environ['PATH']:
         os.environ['PATH'] += os.pathsep + scripts_path
-
-    # Sprawdzenie, czy Django został poprawnie zainstalowany
+        
     try:
         subprocess.check_call(['django-admin', '--version'])
         print("Django został pomyślnie zainstalowany i skonfigurowany.")
